b8c5eb21d249cb5ba800a19e301f620f4d1e3ed9
f1vlad <kulchitski@gmail.com>
2019-08-19 01:41:38 -0700

    Updates

--- resume/config.js
+++
@@ -1,1 +0,0 @@
-IDRViewer.config = {"pagecount":3,"title":"","author":"","fileName":"VladislavKulchitskiResume2019.pdf","bounds":[[935,1210],[935,1210],[935,1210]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
