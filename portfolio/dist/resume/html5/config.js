823234633f941bb3f1d20278c7253535841f4b74
f1vlad <kulchitski@gmail.com>
2020-01-02 22:35:59 -0800

    Updates

--- portfolio/dist/resume/html5/config.js
+++
@@ -1,1 +0,0 @@
-IDRViewer.config = {"pagecount":3,"title":"","author":"","fileName":"VladislavKulchitskiResume2019_(1).pdf","bounds":[[935,1210],[935,1210],[935,1210]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
